ea2151303e060c791deed9b604f3d68ade9a3df4
ntfharry <ntfharry@users.noreply.github.com>
2022-10-19 11:58:32 +0000

    deploy: a79da170a8e883b75e61533a2f1531835621f670

---
+++ _nuxt/static/1666180710/step1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmiho\u002F_nuxt\u002Fstatic\u002F1666180710",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstep1",config:{_app:{basePath:"\u002Fmiho\u002F",assetsPath:"\u002Fmiho\u002F_nuxt\u002F",cdnURL:a}}}}(null));
